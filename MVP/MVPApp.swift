85a4e7603f6958d41761fa09c7421658c34600aa
kayteng <kaylateng@gmail.com>
2024-07-29 15:07:24 -0700

    Initial Commit

---
+++ MVP/MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVPApp.swift
+//  MVP
+//
+//  Created by Scholar on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
