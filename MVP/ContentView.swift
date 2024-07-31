52a1310748e548cf5f97b3b349d6d48239953222
kayteng <kaylateng@gmail.com>
2024-07-31 10:44:45 -0700

    adding views

--- MVP/ContentView.swift
+++ MVP/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack{
-            ZStack{
+            ZStack (alignment: .bottom){
                 Color(red: 247/255, green: 240/255, blue: 233/255)
                     .ignoresSafeArea()
                 VStack {
@@ -355,12 +355,49 @@ struct ContentView: View {
                         }
                         .padding(.horizontal)
                     }
-                    HStack{
-                        
+                    Spacer()
+                    HStack (alignment : .top){
+                        NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                            Image("home icon")
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .padding([.top, .leading, .bottom], 10.0)
+                            .frame(width: 70.0, height: 70.0)}
+                        NavigationLink(destination: infoView()) {
+                            Image("info icon")
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .padding([.top, .leading, .bottom], 10.0)
+                            .frame(width: 70.0, height: 70.0)}
+                        Image("blank")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding([.top, .leading, .bottom], 10.0)
+                            .frame(width: 70.0, height: 70.0)
+                        NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/){
+                            Image("map icon")
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .padding([.top, .leading, .bottom], 10.0)
+                            .frame(width: 70.0, height: 70.0)}
+                        NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                            Image("profile icon")
+                                .resizable(resizingMode: .stretch)
+                                .aspectRatio(contentMode: .fit)
+                                .padding([.top, .bottom, .trailing], 10.0)
+                            .frame(width: 70.0, height: 70.0)}
                     }
                     .background(Rectangle())
-                        .foregroundColor(Color(red: 57/255, green: 60/255, blue: 90/255))
-                    .cornerRadius(15)
+                    .foregroundColor(Color(red: 57/255, green: 60/255, blue: 90/255))
+                    .cornerRadius(45)
+                }
+            Button{
+                    var hi = 1
+            } label: {
+                Image("plus icon")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100.0, height: 100.0)
                 }
             }
         }
---
+++ MVP/infoView.swift
@@ -0,0 +1,65 @@
+//
+//  infoView.swift
+//  MVP
+//
+//  Created by Scholar on 7/31/24.
+//
+
+import SwiftUI
+
+struct infoView: View {
+    var body: some View {
+        NavigationStack{
+            ZStack (alignment: .bottom){
+                Color(red: 247/255, green: 240/255, blue: 233/255)
+                    .ignoresSafeArea()
+                HStack (alignment : .top){
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Image("home icon")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding([.top, .leading, .bottom], 10.0)
+                        .frame(width: 70.0, height: 70.0)}
+                    NavigationLink(destination: infoView()) {
+                        Image("info icon")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding([.top, .leading, .bottom], 10.0)
+                        .frame(width: 70.0, height: 70.0)}
+                    Image("blank")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding([.top, .leading, .bottom], 10.0)
+                        .frame(width: 70.0, height: 70.0)
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/){
+                        Image("map icon")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding([.top, .leading, .bottom], 10.0)
+                        .frame(width: 70.0, height: 70.0)}
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Image("profile icon")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding([.top, .bottom, .trailing], 10.0)
+                        .frame(width: 70.0, height: 70.0)}
+                }
+                .background(Rectangle())
+                .foregroundColor(Color(red: 57/255, green: 60/255, blue: 90/255))
+                .cornerRadius(45)
+                Button{
+                    var hi = 1
+                } label: {
+                    Image("plus icon")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100.0, height: 100.0)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    infoView()
+}
