13348f124931533ad53d4481030e956353821614
kayteng <kaylateng@gmail.com>
2024-08-01 15:06:12 -0700

    info done

---
+++ MVP/mapPopUp.swift
@@ -0,0 +1,18 @@
+//
+//  mapPopUp.swift
+//  MVP
+//
+//  Created by Scholar on 8/1/24.
+//
+
+import SwiftUI
+
+struct mapPopUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mapPopUp()
+}
---
+++ MVP/theFactsView.swift
@@ -0,0 +1,18 @@
+//
+//  theFactsView.swift
+//  MVP
+//
+//  Created by Scholar on 8/1/24.
+//
+
+import SwiftUI
+
+struct theFactsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    theFactsView()
+}
